fd19c8b20117b4267f2349c2fa4b2aeca0f02073
Kapil Rana <kapsrana56@gmail.com>
2024-04-06 11:43:49 +0530

    project-completed

---
+++ src/App.jsx
@@ -0,0 +1,146 @@
+import React from "react";
+import { useState } from "react";
+
+function App() {
+  const [like, setLike] = useState(false);
+  const [likeCount, setLikeCount] = useState(0);
+
+  const likeAndDislike = () => {
+    if (!like) {
+      setLike(true);
+      setLikeCount(likeCount + 1);
+    } else {
+      setLike(false);
+      setLikeCount(likeCount - 1);
+    }
+  };
+  return (
+    <div className=" h-screen flex justify-center items-center bg-pink-100">
+      {/* main */}
+      <div className="main border border-gray-300 rounded-xl shadow-md">
+        {/* Top */}
+        <div className="top">
+          {/* user icon and username */}
+          <div className=" bg-gray-100 rounded-t-xl flex gap-1 py-2 px-2 items-center">
+            <div className="user-icon">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-8 h-8"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                />
+              </svg>
+            </div>
+            <div className="user-name">
+              <h1 className=" text-xl">Kapil Rana</h1>
+            </div>
+          </div>
+        </div>
+        {/* Medieum */}
+        <div className="medieum">
+          {/* Image */}
+          <img
+            onDoubleClick={likeAndDislike}
+            className=" w-[35em] cursor-pointer"
+            src="https://images.unsplash.com/photo-1706125356181-9bff2577579f?&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="img"
+          />
+        </div>
+
+        {/* Bottom */}
+        <div className="bottom">
+          {/* Like Comment share and like text */}
+          <div className="like-comment-share-like_text bg-gray-100 flex justify-between items-center px-2 py-2 rounded-xl">
+            <div className="like-comment-share flex gap-2">
+              {/* like */}
+              {!like ? (
+                <button type="button" onClick={likeAndDislike}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-8 h-8"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+                    />
+                  </svg>
+                </button>
+              ) : (
+                <button type="button" onClick={likeAndDislike}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="red"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-8 h-8 text-red-500"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+                    />
+                  </svg>
+                </button>
+              )}
+
+              {/* comment */}
+              <button>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-8 h-8"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M8.625 9.75a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 0 1 .778-.332 48.294 48.294 0 0 0 5.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
+                  />
+                </svg>
+              </button>
+
+              {/* share */}
+
+              <button>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-8 h-8"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z"
+                  />
+                </svg>
+              </button>
+            </div>
+            <div className="like_text">
+              <p className=" text-lg">{likeCount}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
